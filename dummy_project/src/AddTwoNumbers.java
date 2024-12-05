3a3f1a4749dc45f66876582da210373fc85855c8
DuongUVA <duong.vu.hai@student.uva.nl>
2024-12-05 22:38:01 +0100

    dummy project

---
+++ dummy_project/src/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package dummy_project.src;
+
+public class AddTwoNumbers {
+
+    // Method 1: Adds two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method 2: Multiplies two integers (not a clone)
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ dummy_project/src/SumTwoNumbers.java
@@ -0,0 +1,14 @@
+package dummy_project.src;
+
+public class SumTwoNumbers {
+
+    // Method 1: Adds two integers (Type 1 clone of AddTwoNumbers.add)
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    // Method 2: Subtracts two integers (not a clone)
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
